ce21f53c84f82b99a0e510d470e84f79814f545d
EvergreenTree97 <tkdfhr7497@naver.com>
2021-10-07 00:59:12 +0900

    feat : 갤러리에서 사진 가져와 Display

--- ImageClassifier/app/src/main/java/com/example/imageclassifier/Classifier.java
+++ ImageClassifier/app/src/main/java/com/example/imageclassifier/Classifier.java
@@ -15,6 +15,8 @@ import org.tensorflow.lite.support.common.ops.NormalizeOp;
 import org.tensorflow.lite.support.image.ImageProcessor;
 import org.tensorflow.lite.support.image.TensorImage;
 import org.tensorflow.lite.support.image.ops.ResizeOp;
+import org.tensorflow.lite.support.label.TensorLabel;
+import org.tensorflow.lite.support.model.Model;
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
 
 import java.io.FileInputStream;
@@ -22,6 +24,9 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.channels.FileChannel;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /*
 * 모델 파일 로드
@@ -29,11 +34,13 @@ import java.nio.channels.FileChannel;
 * */
 public class Classifier {
     private static final String MODEL_NAME = "classification.tflite";
+    private static final String LABEL_FILE = "labels.txt";
     private Context context;
-    private Interpreter interpreter = null; //모델에 데이터를 입력하고 추론 결과를 전달받을 수 있는 클래스
+    private Model model;//모델에 데이터를 입력하고 추론 결과를 전달받을 수 있는 클래스 (인터프리터의 확장판)
     private int modelInputWidth, modelInputHeight, modelInputChannel;
     private TensorImage inputImage;
     private TensorBuffer outputBuffer;
+    private List<String> labels;
     /*
     클래스가 생성되는 시점에 context를 적재
      */
@@ -45,34 +52,46 @@ public class Classifier {
     모델 초기화 관련 초기화
      */
     public void init() throws IOException{
-        ByteBuffer model = FileUtil.loadMappedFile(context,MODEL_NAME); //ByteBuffer 포맷으로 얻은 모델
-        model.order(ByteOrder.nativeOrder()); // 시스템의 ByteOrder값과 동일하게 설정
-        interpreter = new Interpreter(model); //스레드 개수, delegate 없이 기본적인 인터프리터 생성
+        model = Model.createModel(context,MODEL_NAME);
         initModelShape();
+        labels = FileUtil.loadLabels(context, LABEL_FILE);
     }
 
     /*
     모델 초기화
      */
     private void initModelShape(){
-        Tensor inputTensor = interpreter.getInputTensor(0);
+        Tensor inputTensor = model.getInputTensor(0);
         int[] inputShape = inputTensor.shape();
         modelInputChannel = inputShape[0]; //1
         modelInputWidth = inputShape[1];  //320
         modelInputHeight = inputShape[2]; //320
         inputImage = new TensorImage(inputTensor.dataType());
-        Tensor outputTensor = interpreter.getOutputTensor(0);
+        Tensor outputTensor = model.getOutputTensor(0);
         outputBuffer = TensorBuffer.createFixedSize(outputTensor.shape(),outputTensor.dataType());
     }
-    public void classify(Bitmap image) {
+    public Pair<String, Float> classify(Bitmap image) {
         inputImage = loadImage(image);
-        interpreter.run(inputImage.getBuffer(), outputBuffer.getBuffer().rewind());
-        argmax(outputBuffer.getFloatArray());
+        Object[] inputs = new Object[]{inputImage.getBuffer()};
+        Map<Integer, Object> outputs = new HashMap();
+        outputs.put(0, outputBuffer.getBuffer().rewind());
+        model.run(inputs,outputs);
+        Map<String,Float> output = new TensorLabel(labels,outputBuffer).getMapWithFloatValue(); //클래스 레이블과 결과 실수를 매핑
+        return argmax(output);
     }
-    private void argmax(float[] array){
-        for(int i = 0 ; i < array.length; i++){
-            Log.d(String.valueOf(i), String.valueOf(array[i]));
+    private Pair argmax(Map<String,Float> map){
+        String maxKey = "";
+        float maxVal = -1;
+        for(Map.Entry<String,Float> entry : map.entrySet()){
+            float f = entry.getValue();
+            if(f > maxVal){
+                maxKey = entry.getKey();
+                maxVal = f;
+
+            }
+            Log.e(entry.getKey(), entry.getValue().toString() );
         }
+        return new Pair<>(maxKey,maxVal);
     }
 
     /*
@@ -80,7 +99,12 @@ public class Classifier {
     Bitmap 이미지를 받아 전처리 후 TensorImage 형태로 변환
      */
     private TensorImage loadImage(final Bitmap bitmap){
-          inputImage.load(bitmap);
+        if(bitmap.getConfig() != Bitmap.Config.ARGB_8888){
+            inputImage.load(convertBitmapToARGB8888(bitmap));
+        }
+        else{
+            inputImage.load(bitmap);
+        }
         ImageProcessor imageProcessor=
                 new ImageProcessor.Builder()
                 .add(new ResizeOp(modelInputWidth,modelInputHeight, ResizeOp.ResizeMethod.NEAREST_NEIGHBOR))
@@ -88,7 +112,9 @@ public class Classifier {
                 .build();
         return imageProcessor.process(inputImage);
     }
-
+    private Bitmap convertBitmapToARGB8888(Bitmap bitmap){
+        return bitmap.copy(Bitmap.Config.ARGB_8888, true);
+    }
 
     /*
     입력받은 이미지 크기를 모델에 맞게 변환
@@ -97,26 +123,8 @@ public class Classifier {
     private Bitmap resizeBitmap(Bitmap bitmap){
         return Bitmap.createScaledBitmap(bitmap,modelInputWidth,modelInputHeight,false);
     }
-    /*
-    ARGB 채널의 이미지를 GrayScale로 변환하고 ByteBuffer 포맷으로 변경
-     */
-    private ByteBuffer convertBitmapToGrayByteBuffer(Bitmap bitmap){
-        ByteBuffer byteByffer = ByteBuffer.allocateDirect(bitmap.getByteCount());
-        byteByffer.order(ByteOrder.nativeOrder());
-        int[] pixels = new int[bitmap.getWidth() * bitmap.getHeight()];
-        bitmap.getPixels(pixels,0,bitmap.getWidth(),0,0,bitmap.getWidth(),bitmap.getHeight());
-        for(int pixel : pixels){
-            int r = pixel >> 16 & 0xFF;
-            int g = pixel >> 8 & 0xFF;
-            int b = pixel & 0xFF;
-
-            float avgPixelValue = (r+g+b) / 3.0f;
-            float nomalizedPixelValue = avgPixelValue/ 255.0f;
-
-            byteByffer.putFloat(nomalizedPixelValue);
-        }
-        return byteByffer;
+    public void finish(){
+        if(model != null) model.close();
     }
-
 }
 
--- ImageClassifier/app/src/main/java/com/example/imageclassifier/GalleryActivity.java
+++ ImageClassifier/app/src/main/java/com/example/imageclassifier/GalleryActivity.java
@@ -1,32 +1,86 @@
 package com.example.imageclassifier;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.ImageDecoder;
 import android.graphics.drawable.BitmapDrawable;
+import android.media.Image;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
+import android.util.Pair;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.io.IOException;
+import java.util.Locale;
 
 public class GalleryActivity extends AppCompatActivity {
+    public static final int GALLERY_IMAGE_REQUEST_CODE = 1;
     private Classifier cls;
     private ImageView imageView;
+    private Button classifyButton;
+    private TextView resultText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
+        init();
+
+
         cls = new Classifier(this);
-        Bitmap icon = BitmapFactory.decodeResource(getApplicationContext().getResources(),R.drawable.glass103);
+        Bitmap icon = BitmapFactory.decodeResource(getApplicationContext().getResources(),R.drawable.paper15);
         try{
             cls.init();
         } catch(IOException e){
             Log.d("DigitClassifier", "failed to init Classifier",e);
         }
-        cls.classify(icon);
-        Log.d("끝", "~~");
+        classifyButton.setOnClickListener(view->{
+            getImageFromGallery();
+        });
 
+    }
+    private void init(){
+         imageView = findViewById(R.id.imageView);
+         classifyButton = findViewById(R.id.classifyButton);
+         resultText = findViewById(R.id.resultText);
+    }
+    private void getImageFromGallery(){
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT).setType("image/*"); // 기기에 저장된 모든 이미지를 가져올 수 있음.
+        startActivityForResult(intent,GALLERY_IMAGE_REQUEST_CODE); //데이터를 전달 받기 위함, 상수 코드 사용
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == Activity.RESULT_OK && requestCode == GALLERY_IMAGE_REQUEST_CODE){
+            if(data == null) return;
+            Uri selectedImage = data.getData();
+            Bitmap bitmap = null;
+            try{
+                if(Build.VERSION.SDK_INT>=29){
+                    ImageDecoder.Source src = ImageDecoder.createSource(getContentResolver(),selectedImage);
+                    bitmap = ImageDecoder.decodeBitmap(src);
+                }else{ //
+                    bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),selectedImage);
+                }
+            }catch(IOException e){
+                Log.e("Failed to read Image", e.toString());
+            }
+            if(bitmap != null){
+                Pair<String,Float> output = cls.classify(bitmap);
+                String resultStr = String.format(Locale.ENGLISH,"클래스 : %s\n정확도 : %.2f%%",output.first,output.second*100);
+                imageView.setImageBitmap(bitmap);
+                resultText.setText(resultStr);
+            }
+        }
     }
 }
